239934fbca075db54f578effebc264223e580b6a
cpainagent007 <cpainagent007@gmail.com>
2025-04-02 14:45:18 -0600

    Added .h and new .cpp files for Student

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,8 +1,18 @@
+
+#include "Student.h"
 #include <iostream>
+
 using namespace std;
 
 int main() {
+    Student student;
+    student.SetName("Test");
+    student.Write();
+
+    f();
+
     cout << "Hello, World!" << endl;
     cout << "Git is now tracking this project!" << endl;
+
     return 0;
 }
