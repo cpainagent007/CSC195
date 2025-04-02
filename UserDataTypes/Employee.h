f0acb3c27df19c4d89d4c415d800d7ac6808adb2
cpainagent007 <cpainagent007@gmail.com>
2025-04-02 17:10:30 -0600

    Added Employee class for Payroll Program

---
+++ UserDataTypes/Employee.h
@@ -0,0 +1,19 @@
+
+#pragma once
+#include <iostream>
+#include <string>
+
+class Employee {
+public:
+    void Read();
+    void Write();
+
+private:
+    std::string name;
+    short age;
+    unsigned int zipcode;
+    float wage;
+    float hoursWorked;
+
+    static const float TAX;
+};
