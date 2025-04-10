63e35e0e7ecae8a31e0a78ec3c7bd56728e2b578
cpainagent007 <cpainagent007@gmail.com>
2025-04-10 14:20:10 -0600

    Pointers vs. References in References.cpp

--- References/References.cpp
+++ References/References.cpp
@@ -1,7 +1,71 @@
 
 #include <iostream>
 
+using namespace std;
+
+void func1(int* p)
+{
+	if (p != nullptr) {
+		*p = 10;
+	}
+	else {
+		cout << "Pointer is null" << endl;
+	}
+}
+
+//Using pointers to swap values (Does not modify the original variables, passed by value)
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+//Using references to swap values (Also modifies the original variables, passed by memory)
+void swap(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Pointers are used to store the address of a variable, and can be null
+	//References are used to create an alias for a variable, and cannot be null
+
+	string breakPoint = "----------------------------";
+
+	int i1 = 5;
+	int i2 = 20;
+	int* p1 = &i1;
+
+	//Dereferencing a pointer
+	p1 = nullptr;
+	func1(p1);
+
+	//Creating a reference to i1;
+	int& r1 = i1;
+	cout << r1 << endl;
+	cout << i1 << endl;
+
+	cout << &r1 << endl;
+
+	//----------
+	cout << breakPoint << endl;
+	//----------
+
+	//Swapping difference with pointers and references
+
+	//Pointers (using & to get the address)
+	cout << "i1: " << i1 << " i2: " << i2 << endl;
+	swap(&i1, &i2);
+	cout << "i1: " << i1 << " i2: " << i2 << endl;
+
+	//----------
+	cout << breakPoint << endl;
+	//----------
+
+	//References (no & to get the address, it happens in the function)
+	cout << "i1: " << i1 << " i2: " << i2 << endl;
+	swap(i1, i2);
+	cout << "i1: " << i1 << " i2: " << i2 << endl;
 }
